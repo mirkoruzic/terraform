c212e53295c8e0251b955ea5c9b068acff2742fb
mirkoruzic <mirko.ruzic@live.com>
2024-02-21 16:01:38 +0100

    Update main.tf

--- aws/project_abc/environments/dev/application/main.tf
+++ aws/project_abc/environments/dev/application/main.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket = "[SET_S3_BUCKET_FOR_TF_STATE]"  # Replace with your Terraform S3 TFState bucket
+    bucket = [SET_S3_BUCKET_FOR_TF_STATE]  # Replace with your Terraform S3 TFState bucket
     key    = "state/environment/${var.env}/network/terraform.tfstate"
     region = "eu-west-1"
     encrypt = true
